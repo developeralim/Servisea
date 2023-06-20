1b39ead345280d6ea31c725c40a65169d1934b54
muha18 <mbengah@umail.utm.ac.mu>
2023-06-20 20:26:43 +0400

    n

--- Servisea/servisea/app/Models/Order.php
+++ Servisea/servisea/app/Models/Order.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    public $timestamps = false;
+    protected $table = 'order';
+    protected $fillable = [
+        'PACKAGE_ID',
+        'USER_ID',
+        'FREELANCER_ID',
+        'PAYMENT_ID',
+        'ORDER_AMOUNT',
+        'ORDER_DATE',
+        'ORDER_DUE',
+        'ORDER_STATUS',
+    ];
 }
