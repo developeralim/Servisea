c682381b70dd1f8a9632a6ce7708a911b403d902
muha18 <mbengah@umail.utm.ac.mu>
2023-06-21 11:32:24 +0400

    k

--- Servisea/servisea/app/Http/Controllers/Custom/stripeController.php
+++ Servisea/servisea/app/Http/Controllers/Custom/stripeController.php
@@ -7,18 +7,38 @@ use Illuminate\Http\Request;
 
 class stripeController extends Controller
 {
-
-
-    public function index(){
-
+    public function checkout()
+    {
+        return view('checkout');
     }
 
-    public function checkout(){
+    public function session()
+    {
+        \Stripe\Stripe::setApiKey(config('stripe.sk'));
 
+        $session = \Stripe\Checkout\Session::create([
+            'line_items'  => [
+                [
+                    'price_data' => [
+                        'currency'     => 'gbp',
+                        'product_data' => [
+                            'name' => 'gimme money!!!!',
+                        ],
+                        'unit_amount'  => 500,
+                    ],
+                    'quantity'   => 1,
+                ],
+            ],
+            'mode'        => 'payment',
+            'success_url' => route('success'),
+            'cancel_url'  => route('checkout'),
+        ]);
+
+        return redirect()->away($session->url);
     }
 
-    public function success(){
-
+    public function success()
+    {
+        return "Yay, It works!!!";
     }
-
 }
